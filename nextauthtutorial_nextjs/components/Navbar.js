0da6d1139f3c477be2585c805f06d8ce25ffc097
DonMizzy <131966450+DonMizzy@users.noreply.github.com>
2024-02-03 15:53:14 +0100

    true

--- nextauthtutorial_nextjs/components/Navbar.js
+++ nextauthtutorial_nextjs/components/Navbar.js
@@ -1,60 +1,57 @@
-import Link from "next/link"
+import Link from "next/link";
 import { useSession, signIn, signOut } from "next-auth/react";
 
 function Navbar() {
-    const  { data: session, loading } = useSession();
-    console.log({ session, loading });
+  const { data: session, loading } = useSession();
+  console.log({ session, loading });
   return (
-      <nav className="header">
-          <h1 className="logo">
-              <a href="#">NextAuth</a>
-          </h1>
+    <nav className="header">
+      <h1 className="logo">
+        <a href="#">NextAuth</a>
+      </h1>
 
-          <ul className={`main-nav ${!session && loading ? 'loading' : 'loaded'}`}>
+      <ul className={`main-nav ${!session && loading ? "loading" : "loaded"}`}>
+        <li>
+          <Link href="/">Home</Link>
+        </li>
+        <li>
+          <Link href="/dashboard">Dashboard</Link>
+        </li>
+        <li>
+          <Link href="/Blog">Blog</Link>
+        </li>
+        {!loading && !session && (
           <li>
-              <Link href="/">
-                  Home
-              </Link>
+            <Link href="/api/auth/signin">
+              <span
+                onClick={(e) => {
+                  e.preventDefault();
+                  signIn();
+                }}
+              >
+                Sign up{" "}
+              </span>
+            </Link>
           </li>
+        )}
+
+        {session && (
           <li>
-              <Link href="/dashboard">
-                  Dashboard
-              </Link>
+            <Link href="/api/auth/signout">
+              <span
+                onClick={(e) => {
+                  e.preventDefault();
+                  signOut();
+                }}
+              >
+                Sign Out
+              </span>
+            </Link>
           </li>
-          <li>
-              <Link href="/Blog">
-                  Blog
-              </Link>
-              </li>
-              {
-                  !loading && !session && (
-                      <li>
-                  <Link href="/api/auth/signin"
-                    
-                  >
-                    <span  onClick={e => {
-                      e.preventDefault()
-                      signIn("github")
-                      }}>Sign In  </span> 
-              </Link>
-          </li>
-                  )
-              }
-          
-              {session && (
-                  <li>
-              <Link href="/api/auth/signout">
-                      <span onClick={e => {
-                          e.preventDefault()
-                          signOut()
-                      }}>Sign Out</span> 
-              </Link>
-              </li>
-              )}
-          
-          </ul>
+        )}
+      </ul>
     </nav>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
